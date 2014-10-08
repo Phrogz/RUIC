fd764141c8e72c55b47467ebae80126c504ee0e3
Gavin Kistner <gavin@phrogz.net>
2014-10-08 11:07:38 -0600

    Fix UTF-8 from .ui coming through

--- gui/launch.rb
+++ gui/launch.rb
@@ -1,5 +1,12 @@
+#encoding: utf-8
 require 'Qt'
 require_relative 'window'
+
+class Qt::Application
+	def self.translate(*a,&b)
+		method_missing(:translate,*a,&b).force_encoding('utf-8')
+	end
+end
 
 $app = Qt::Application.new(ARGV)
 UIC::GUI.new.show
--- gui/window.rb
+++ gui/window.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 require_relative 'window_qrc'
 require_relative '../lib/ruic'
 
